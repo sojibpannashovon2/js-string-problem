811c2bd0de76d8d979ce2149f883aba27641ae8e
Md Arifur Rahman Sojib <sojibkhanarif999@gmail.com>
2022-08-11 11:25:31 +0600

    lowerCase and includes

---
+++ search.js
@@ -0,0 +1,17 @@
+
+const lyrics = 'I wanna love every moment of my Life, Because you are my creator'
+
+
+const word = 'Love';
+
+const wordM = word.toLowerCase();
+
+console.log(wordM);
+
+const lyricsM = lyrics.toLowerCase();
+
+console.log(lyricsM);
+
+const doesExistM = lyricsM.includes(wordM);
+
+console.log(doesExistM);
